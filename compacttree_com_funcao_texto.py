33ba225022495480ca6b7e1fd1a25bd255f35d23
Tiago Alves <tiago_alves26@hotmail.com>
2015-06-03 17:30:50 +0100

    upd..
    ja encurta a arvore apartir dos nos, logo... agora falta resolver o
    problema do algoritmo de pesquisa que ainda nao funciona

--- compacttree_com_funcao_texto.py
+++ compacttree_com_funcao_texto.py
@@ -5,6 +5,10 @@ Created on Tue May 26 20:02:54 2015
 @author: Tiago
 """
 
+from Bio import Entrez
+from Bio import SeqIO
+Entrez.email='tiago_alves26@hotmail.com'
+
 class CreateTree:
     
     def __init__(self):
@@ -49,7 +53,7 @@ class CreateTree:
         for i in range (len(t)):
             self.addSufix(t[i:],i)
             
-
+######################################################################################################3
     def nobiforca(self,node=0):#procura por caminhos nao bifurcados dado um nodo.
         if len(self.nodes[node][1].keys())==0:#if len(self.nodes[node][1].keys())==0:#atingimos uma folha
             res=int(self.nodes[node][0])#numero da folha colocado na lista
@@ -106,7 +110,7 @@ class CreateTree:
                         word=str(self.nodes[node][0])
                         #insere a letra na ultima posicao da lista (o texto vai do fim para o inicio)
         print word
-             ############################
+          ####################################################################   ############################
                 
                 
 #para saber se é biforcado ou nao, dado o no respetivo
@@ -122,23 +126,43 @@ class CreateTree:
                 return False
         except:
             print "errado"
+            return False###########
         
 #funcao que retirao texto, dados os nos iniciais e finais do ramo em questao
         
     def text_with_cut (self,init,final):#distancia entre nos inicial e final
         text=""
-        letter=self.nodes[init][1].keys()[0]
-        text+=letter
-        destin0=self.nodes[init][1].values()
-        init_cicle_node=destin0[0]
-        while init_cicle_node!=final:
-            letter2=self.nodes[init_cicle_node][1].keys()[0]
-            text+=letter2
-            destin=self.nodes[init_cicle_node][1].values()
-            destin_node=destin[0]
-            del self.nodes[init_cicle_node]            
-            init_cicle_node=destin_node
+        #sdicio={}
+        keys=self.nodes[init][1].keys()
+        if len(keys)>1:
+            for key in keys:
+                next_node=self.nodes[init][1][key]
+                final_node=self.getLastNodeBelow(next_node)#vai ver se o no final é o mesmo que é passado pela funcao anterior. Se o no seguinte for logo um biforcado não ocorre união de texto uma vez que so podiamos juntar uma letra apenas
+                if final_node[0]==final:
+                    text+=key
+                    while next_node!=final:
+                        letter2=self.nodes[next_node][1].keys()[0]
+                        text+=letter2
+                        destin=self.nodes[next_node][1].values()
+                        destin_node=destin[0]
+                        del self.nodes[next_node]            
+                        next_node=destin_node
+ #acho que isto nao sera preciso uma vez que a seguinte apaga tudo mas confirmar  #del self.nodes[init][1][key]#apaga a chave correspondente ao caminho do no biforcado em estudo
+                    #self.nodes[init][1][]=
+                
         #if len(self.nodes[final][1].keys())==0:
+        elif len(keys)==1:
+            text+=key[0]
+            next_node=self.nodes[init][1][keys[0]]
+     #########       del self.nodes[init]#apaga o no inicial
+            while next_node!=final:
+                letter2=self.nodes[next_node][1].keys()[0]
+                text+=letter2
+                destin=self.nodes[next_node][1].values()
+                destin_node=destin[0]
+                del self.nodes[next_node]            
+                next_node=destin_node
+         #if len(self.nodes[final][1].keys())==0:
          #   text+="$"
         #texto=text.strip(",")#em principio nao sera necessario uma vez que foi usada a concatenacao de strings
         #if len(texto)>=2:
@@ -154,13 +178,13 @@ class CreateTree:
         dicio={}        
         #if self.biforcado(node):
         try:
-            nos=self.getLastNodeBelow(node)
+            nos=self.getLastNodeBelow_main(node)
             #print nos
-            for stop in nos:
+            for stop in range(len(nos)):
                 #print stop
-                text=self.text_with_cut(node,stop)
+                text=self.text_with_cut(node,nos[stop])
                 #print text
-                dicio[text]=stop
+                dicio[text]=nos[stop]
                 #eliminar o dicionario existente    
             self.nodes[node][1].clear()
             for key in dicio.keys():
@@ -189,11 +213,29 @@ class CreateTree:
           #  print newnode
             stop=self.getLastNodeBelow(newnode[0])
             res.extend(stop)         
-        else:
+        else:#se for biforcado
             res.append(node)
         #print res
         return res
         
+
+#imprime ou um ou varios nos finais
+    
+    def getLastNodeBelow_main(self,no):#biforcado ou nao
+        res=[]
+        keys=self.nodes[no][1].keys()
+        print keys        
+        if len(keys)==1:#se nao for biforcado
+            final_node=self.getLastNodeBelow(no)
+            res.extend(final_node)
+        elif len(keys)>1:#biforcado
+            for key in keys:
+                next_node=self.nodes[no][1][key]
+                print next_node                
+                final_node_bifor=self.getLastNodeBelow(next_node)
+                res.extend(final_node_bifor)
+        return res
+    
     
    # def direct_line_to_leaf(self,node):
         
@@ -221,7 +263,7 @@ class CreateTree:
                         node=self.nodes[num][1][key]
                         print node
                         self.encurtar(node)
-                if not bifor:
+                elif not bifor:
                     self.encurtar(num)
                 else:
                     pass
@@ -234,7 +276,8 @@ class CreateTree:
             #uma vez que so juntam o texto apartir do no seguinte de cada bifurcacao, 
             #deixando um no que podia ser eliminado para tras 
             #(fica o seguinte a bifurcacao e o final e podia ficar logo que se da a bifurcacao ligado ao final)
-#para isso tem de se adaptar a text_with_cut ou criar outra que chame essa e junte o caracter inicial ao texto que sai dessa- ter atencao que se tem de eliminar o nodo)
+#para isso tem de se adaptar a text_with_cut ou criar outra que chame essa e junte o caracter inicial ao texto 
+            #que sai dessa- ter atencao que se tem de eliminar o nodo)
         #a adaptar a funcao getlastnodebelow, uma vez que tem de permitir nos iniciais biforcados e dar um output de
             #quais os destinos da funcao cut
             
@@ -324,6 +367,178 @@ class CreateTree:
 ###Retirar as sequencias das bases de dados (padrão-gene) e sufixos (genoma)###
 ###############################################################################        
       
+#vamos supor que a arvore esta a funcionar direitinho... PAsso seguinte:
+      
+      
+      
+    def get_seq_from_genome(self,especie=0,idn=0):
+        if idn==0:
+            gf=self.get_genome_file(especie)
+         #   f=open(gf+".fasta", 'r+')
+        #    sequence = SeqIO.read(f, 'fasta')
+       #     print str(sequence.seq).strip(" ")
+        elif especie==0:
+            gf=self.get_genome_file(0,idn)
+            print gf
+        f=open(gf, 'r')
+        sequence = SeqIO.read(f, 'fasta')
+        #print str(sequence.seq).strip(" ")
+        print "A sequencia usada para construir a arvore sera:\n"+str(sequence.id)#para informar qual sera a sequencia usada
+        return str(sequence.seq).strip(" ")
+        
+        
+
+       
+       
+       
+    def get_genome_file(self,especie=0,idn=0):#cria o ficheiro e da o nome a funcao anterior para retirar a sequencia do ficheiro
+        return_filename=""        
+        
+        if idn==0:
+            hand=Entrez.esearch(db='nucleotide',term=especie+"[ORGN]",retmax=100,retype="gb",retmode="text")
+            results=Entrez.read(hand)
+            idnum=results["IdList"][0]#primeiro elemento da lista de resultados
+            print idnum
+            
+            handle=Entrez.efetch(db='nucleotide',rettype="fasta",retmode="text",id=idnum)
+     #       record=Entrez.read(handle)
+            #print(record[0].keys())
+            #print (record[0]["TSeq_defline"])
+            #print handle.read()
+            
+            
+            read=SeqIO.read(handle,"fasta")
+            name="genome_"+str(especie).strip(" ")+".fasta"
+            SeqIO.write(read,name, "fasta")
+            handle.close()
+            return_filename+=name
+  ### #         record = SeqIO.read("genome_escherichia coli.fasta", "fasta")
+  ###  #        print record             
+           # filename = "genome_"+especie+".gb"
+        
+        elif especie==0:
+            handle=Entrez.efetch(db='nucleotide',rettype="fasta",retmode="text",id=idn)
+            read=SeqIO.read(handle,"fasta")
+            name="genome_"+str(idn).strip(" ")+".fasta"
+            SeqIO.write(read,name,"fasta")
+            handle.close()
+            return_filename+=name
+        #print return_filename
+        return return_filename
+        
+
+        #f=open(filename,"w+")
+        #f.write(handle.read())    
+        #print handle.read()
+        #return record["IdList"]#especie
+        #rint record
+        
+    
+        
+        
+       
+       
+       
+    def get_seq_from_gene(self,especie=0,idn=0):
+        if idn==0:
+            gf=self.get_genome_file(especie)
+         #   f=open(gf+".fasta", 'r+')
+        #    sequence = SeqIO.read(f, 'fasta')
+       #     print str(sequence.seq).strip(" ")
+        elif especie==0:
+            gf=self.get_genome_file(0,idn)
+            print gf
+        f=open(gf, 'r')
+        sequence = SeqIO.read(f, 'fasta')
+        #print str(sequence.seq).strip(" ")
+        print "A sequencia usada para construir a arvore sera:\n"+str(sequence.id)#para informar qual sera a sequencia usada
+        return str(sequence.seq).strip(" ")
+        
+        
+
+       
+       
+       
+    def get_gene_file(self,gene=0,idn=0):#cria o ficheiro e da o nome a funcao anterior para retirar a sequencia do ficheiro
+        return_filename=""        
+        
+        if idn==0:
+            hand=Entrez.esearch(db='gene',term=gene+"[sym]",retmax=100,retype="gb",retmode="text")
+            results=Entrez.read(hand)
+            idnum=results["IdList"][0]#primeiro elemento da lista de resultados
+            print idnum
+   
+        
+            handl=Entrez.efetch(db='gene',rettype="gb",retmode="xml",id=idnum)
+            record=Entrez.read(handl)
+            #print(record[0].keys())#["Entrezgene_summary"])
+            to=record[0]['Entrezgene_comments'][5]['Gene-commentary_comment'][0]['Gene-commentary_comment'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']['Seq-interval']['Seq-interval_to']#para
+
+            desde=record[0]['Entrezgene_comments'][5]['Gene-commentary_comment'][0]['Gene-commentary_comment'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']['Seq-interval']['Seq-interval_from']#de
+
+            identif=record[0]['Entrezgene_comments'][5]['Gene-commentary_comment'][0]['Gene-commentary_comment'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']['Seq-interval']['Seq-interval_id']['Seq-id']['Seq-id_gi']#gi
+                        
+            handle = Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id=identif, seq_start=desde, seq_stop=to )
+            #print (record[0]["TSeq_defline"])
+            #print handle.read()
+            
+        #[u'Entrezgene_unique-keys', u'Entrezgene_track-info', u'Entrezgene_gene', u'Entrezgene_location', 
+            #u'Entrezgene_xtra-properties', u'Entrezgene_locus', u'Entrezgene_homology', u'Entrezgene_source', 
+         #   u'Entrezgene_type', u'Entrezgene_gene-source', u'Entrezgene_comments', u'Entrezgene_xtra-index-terms', 
+          #  u'Entrezgene_properties', u'Entrezgene_prot', u'Entrezgene_summary']   
+           
+           
+           
+            read=SeqIO.read(handle,"fasta")
+            name="gene_"+str(gene).strip(" ")+".fasta"
+            SeqIO.write(read,name, "fasta")
+            handle.close()
+            return_filename+=name
+  ### #         record = SeqIO.read("genome_escherichia coli.fasta", "fasta")
+  ###  #        print record             
+           # filename = "genome_"+especie+".gb"
+        
+        elif gene==0:
+            handl=Entrez.efetch(db='gene',rettype="gb",retmode="xml",id=idn)
+            record=Entrez.read(handl)
+            #print(record[0].keys())#["Entrezgene_summary"])
+            to=record[0]['Entrezgene_comments'][5]['Gene-commentary_comment'][0]['Gene-commentary_comment'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']['Seq-interval']['Seq-interval_to']#para
+
+            desde=record[0]['Entrezgene_comments'][5]['Gene-commentary_comment'][0]['Gene-commentary_comment'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']['Seq-interval']['Seq-interval_from']#de
+
+            identif=record[0]['Entrezgene_comments'][5]['Gene-commentary_comment'][0]['Gene-commentary_comment'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']['Seq-interval']['Seq-interval_id']['Seq-id']['Seq-id_gi']#gi
+                        
+            handle = Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id=identif, seq_start=desde, seq_stop=to )
+            #print (record[0]["TSeq_defline"])
+            #print handle.read()
+            
+        #[u'Entrezgene_unique-keys', u'Entrezgene_track-info', u'Entrezgene_gene', u'Entrezgene_location', 
+            #u'Entrezgene_xtra-properties', u'Entrezgene_locus', u'Entrezgene_homology', u'Entrezgene_source', 
+         #   u'Entrezgene_type', u'Entrezgene_gene-source', u'Entrezgene_comments', u'Entrezgene_xtra-index-terms', 
+          #  u'Entrezgene_properties', u'Entrezgene_prot', u'Entrezgene_summary']   
+           
+           
+           
+            read=SeqIO.read(handle,"fasta")
+            name="gene_"+str(idn).strip(" ")+".fasta"
+            SeqIO.write(read,name, "fasta")
+            handle.close()
+            return_filename+=name
+        #print return_filename
+      #  return return_filename
+        
+
+        #f=open(filename,"w+")
+        #f.write(handle.read())    
+        #print handle.read()
+        #return record["IdList"]#especie
+        #rint record
+        
+    
+        
+        
+       
+
       
       
       
@@ -331,9 +546,7 @@ class CreateTree:
       
       
       
-      
-      
-      
+from Bio import Entrez      
       
 ##############################################################3
 if __name__=='__main__':
@@ -356,9 +569,16 @@ if __name__=='__main__':
         #print st.text_with_cut(6,9)
         #print st.getLastNodeBelow(2)
         #print st.encurtar(2)
-        print st.s_to_c_tree()
+        #print st.s_to_c_tree()
         #print st.text_with_cut(2,5)
        # print st.biforcado(2)
-        print st.findPattern("")
-   
+        #print st.findPattern("")
+       # print st.pesquisa_genoma()
+        #a=Entrez.einfo()
+        #print (Entrez.read(a))
+        #print st.get_genome_file("escherichia coli")
+        #print st.get_gene_file("idh1")#id tem de ser da bd gene
+        print st.getLastNodeBelow_main(1)
+    
+    
     test2()
